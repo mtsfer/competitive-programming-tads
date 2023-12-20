be7883a330d1befac3b65d900a505c5cfcfb0b1d
Mateus Ferraz <mtshs.ferraz@gmail.com>
2023-12-19 23:51:07 -0300

    Solve 'Busca-Palavras' (OPEI2023-SENIOR-CANCELED) problem

---
+++ opei_2023_senior_cancelled/busca_palavras.py
@@ -0,0 +1,75 @@
+MATRIX_SIZE = 6
+EMPTY_PLACE_SYMBOL = '-'
+CURRENT_POSITION_SYMBOL = "#"
+
+
+def print_matrix(matrix):
+    for i in range(MATRIX_SIZE):
+        for j in range(MATRIX_SIZE):
+            print(matrix[i][j], end=' ')
+        print()
+
+
+def get_next_coordinate(current_x, current_y, letter_x, letter_y):
+    # destination coordinate - current coordinate:
+    # (> 0, > 0) => go to the next place in first diagonal
+    # (> 0, < 0) => go to the previous place in first diagonal
+    # (< 0, > 0) => go to the next place in second diagonal
+    # (< 0, < 0) => go to the previous place in second diagonal
+    x_difference = letter_x - current_x
+    y_difference = letter_y - current_y
+
+    if x_difference > 0:
+        current_x += 1
+    elif x_difference < 0:
+        current_x -= 1
+    if y_difference > 0:
+        current_y += 1
+    elif y_difference < 0:
+        current_y -= 1
+
+    return current_x, current_y
+
+
+def main():
+    matrix = [[EMPTY_PLACE_SYMBOL for _ in range(MATRIX_SIZE)] for _ in range(MATRIX_SIZE)]
+
+    current_x, current_y = map(int, input().split())
+    matrix[current_x][current_y] = CURRENT_POSITION_SYMBOL
+
+    word_size = int(input())
+    letters_info = []
+    for _ in range(word_size):
+        letter, letter_x, letter_y = input().split()
+        letter_x = int(letter_x)
+        letter_y = int(letter_y)
+        matrix[letter_x][letter_y] = letter
+        letters_info.append([letter, letter_x, letter_y])
+
+    print(f'Inicialmente nossa matriz e:')
+    print_matrix(matrix)
+
+    word_collected = ''
+    next_letter_index_to_search = 0
+
+    while next_letter_index_to_search < word_size:
+        current_letter_info = letters_info[next_letter_index_to_search]
+        letter_x = current_letter_info[1]
+        letter_y = current_letter_info[2]
+
+        matrix[current_x][current_y] = EMPTY_PLACE_SYMBOL
+        current_x, current_y = get_next_coordinate(current_x, current_y, letter_x, letter_y)
+        matrix[current_x][current_y] = CURRENT_POSITION_SYMBOL
+
+        if [current_x, current_y] == [letter_x, letter_y]:
+            word_collected += current_letter_info[0]
+            next_letter_index_to_search += 1
+
+        print(f'Por enquanto temos a palavra: {word_collected}')
+        print_matrix(matrix)
+
+    print(f'Acabamos nossa jornada e temos a palavra: {word_collected}')
+
+
+if __name__ == '__main__':
+    main()
