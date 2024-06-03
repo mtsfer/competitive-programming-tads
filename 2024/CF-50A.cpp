6f1fc45e6ef0eb0d7f5a46d04b6907e7d577ee0b
mtsfer <mtshs.ferraz@gmail.com>
2024-06-02 22:47:53 -0300

    Solve 'Domino Piling' (CF-50A) problem

---
+++ 2024/CF-50A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int m, n;
+    cin >> m >> n;
+    int qtt = m * n / 2;
+    cout << qtt << "\n";
+
+    return 0;
+}
