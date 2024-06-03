aeb67ca00760df6be8f6b673f2d196b327a95ff4
mtsfer <mtshs.ferraz@gmail.com>
2024-06-02 22:40:46 -0300

    Solve 'Watermelon' (CF-4A) problem

---
+++ 2024/CF-4A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int w;
+    cin >> w;
+
+    if (w == 2 || w % 2 == 1) {
+        cout << "NO\n";
+    }
+    else {
+        cout << "YES\n";
+    }
+
+    return 0;
+}
