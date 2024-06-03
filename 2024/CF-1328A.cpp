fdb7f7783bb5329a0a55a103ef45a3ddf2e757b6
mtsfer <mtshs.ferraz@gmail.com>
2024-06-03 00:18:25 -0300

    Solve 'Divisibility Problem' (CF-1328A) problem

---
+++ 2024/CF-1328A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        if (a % b == 0) {
+            cout << 0 << "\n";
+        }
+        else {
+            cout << b - (a % b) << "\n";
+        }
+    }
+
+    return 0;
+}
